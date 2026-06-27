ef97935325e7346d89d1147e1c2a544b02235655
cr3atee <mit111app@gmail.com>
2026-06-27 16:12:53 +0700

    chore(ui): remove unused DOM compatibility shim

--- src/core/domCollectionCompat.js
+++
@@ -1,8 +0,0 @@
-export function initDomCollectionCompat() {
-  const prototype = globalThis.HTMLOptionsCollection?.prototype;
-  if (!prototype || typeof prototype.map === 'function') return;
-  Object.defineProperty(prototype, 'map', {
-    configurable: true,
-    value: Array.prototype.map
-  });
-}
