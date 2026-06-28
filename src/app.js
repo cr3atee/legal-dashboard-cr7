6d5a867290c57e3f687a593e0ee37358059d80d3
cr3atee <mit111app@gmail.com>
2026-06-28 20:32:26 +0700

    Initialize restored quarterly report controller

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ import { initMeetingsPage } from './modules/meetings/meetingsController.js';
 import { initMeetingsWorkflowUi } from './modules/meetings/meetingsWorkflowUi.js';
 import { initReportsPage } from './modules/reports/reportsController.js';
 import { initReportsCalendarDetails } from './modules/reports/reportsCalendarDetails.js';
+import { initReportsQuarterController } from './modules/reports/reportsQuarterController.js';
 import { initAdminUsersPage } from './modules/adminUsers/adminUsersController.js';
 import { initAdminDictionariesPage } from './modules/adminDictionaries/adminDictionariesController.js';
 import { initMapFullscreenButton } from './modules/map/mapFullscreen.js';
@@ -58,6 +59,7 @@ export function initApp() {
     initMeetingsWorkflowUi();
     initReportsPage();
     initReportsCalendarDetails();
+    initReportsQuarterController();
     initAdminUsersPage();
     initAdminDictionariesPage();
     initMapFullscreenButton();
