1a8e8f85f131fe5cef0c3462b74837ca5f52d70c
cr3atee <mit111app@gmail.com>
2026-07-01 20:06:38 +0700

    Подключить интерфейс отменённых дел

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import { initCaseUiEnhancements } from './modules/cases/caseUiEnhancements.js';
 import { initAppealCalculatorUiFix } from './modules/cases/appealCalculatorUiFix.js';
 import { initAppealTaskDateBridge } from './modules/cases/appealTaskDateBridge.js';
 import { initAppealActionCleanup } from './modules/cases/appealActionCleanup.js';
+import { initGeneralCaseCancellationUi } from './modules/cases/generalCaseCancellationUi.js';
 import { initLegalFieldSuggestions } from './modules/common/legalFieldSuggestions.js';
 import { initGeneralCasesPage } from './modules/cases/generalCasesController.js';
 import { initControlledCasesPage } from './modules/controlledCases/controlledCasesController.js';
@@ -50,6 +51,7 @@ export function initApp() {
     initAppealActionCleanup();
     initLegalFieldSuggestions();
     initGeneralCasesPage();
+    initGeneralCaseCancellationUi();
     initControlledCasesPage();
     initEnforcementPage();
     initCalendarSelectedUserOnly();
