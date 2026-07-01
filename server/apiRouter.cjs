44090d63b707ef10c9a1ab2acd93984c58482bef
cr3atee <mit111app@gmail.com>
2026-07-01 20:06:15 +0700

    Подключить маршруты отменённых дел

--- server/apiRouter.cjs
+++ server/apiRouter.cjs
@@ -4,16 +4,19 @@ const { handleReportMetricsWrite } = require('./reportMetricsWriteRoute.cjs');
 const { ensureCaseSuggestionSeeds } = require('./caseSuggestionSeeds.cjs');
 const { handleCalendarUsers } = require('./calendarUsersRoute.cjs');
 const { ensureGeneralCaseNumberSchema, handleGeneralCaseNumber } = require('./generalCaseNumberRoute.cjs');
+const { ensureGeneralCaseCancellationSchema, handleGeneralCaseCancellation } = require('./generalCaseCancellationRoute.cjs');
 
 async function ensureSchema(dbPath) {
   await core.ensureSchema(dbPath);
   await ensureCaseSuggestionSeeds(dbPath);
   await ensureReportMetricsSchema(dbPath);
   await ensureGeneralCaseNumberSchema(dbPath);
+  await ensureGeneralCaseCancellationSchema(dbPath);
 }
 
 async function handleApiRequest(req, res, url, dbPath) {
   if (await handleGeneralCaseNumber(req, res, url, dbPath)) return true;
+  if (await handleGeneralCaseCancellation(req, res, url, dbPath)) return true;
   if (await handleCalendarUsers(req, res, url, dbPath)) return true;
   if (await handleReportMetrics(req, res, url, dbPath)) return true;
   if (await handleReportMetricsWrite(req, res, url, dbPath)) return true;
