64d39205e25a47b852faf321115772217f0e20c6
cr3atee <mit111app@gmail.com>
2026-07-03 16:00:04 +0700

    Подключить удаление связанных архивов дел

--- server/apiRouter.cjs
+++ server/apiRouter.cjs
@@ -5,6 +5,7 @@ const { ensureCaseSuggestionSeeds } = require('./caseSuggestionSeeds.cjs');
 const { handleCalendarUsers } = require('./calendarUsersRoute.cjs');
 const { ensureGeneralCaseNumberSchema, handleGeneralCaseNumber } = require('./generalCaseNumberRoute.cjs');
 const { ensureGeneralCaseCancellationSchema, handleGeneralCaseCancellation } = require('./generalCaseCancellationRoute.cjs');
+const { handleLinkedCaseLifecycle } = require('./linkedCaseLifecycleRoute.cjs');
 
 async function ensureSchema(dbPath) {
   await core.ensureSchema(dbPath);
@@ -17,6 +18,7 @@ async function ensureSchema(dbPath) {
 async function handleApiRequest(req, res, url, dbPath) {
   if (await handleGeneralCaseNumber(req, res, url, dbPath)) return true;
   if (await handleGeneralCaseCancellation(req, res, url, dbPath)) return true;
+  if (await handleLinkedCaseLifecycle(req, res, url, dbPath)) return true;
   if (await handleCalendarUsers(req, res, url, dbPath)) return true;
   if (await handleReportMetrics(req, res, url, dbPath)) return true;
   if (await handleReportMetricsWrite(req, res, url, dbPath)) return true;
